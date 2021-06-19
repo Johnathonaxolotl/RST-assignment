2bba3513f11f2217e1787568302a5aad8627cc6a
Johnathonaxolot <83236279+Johnathonaxolotl@users.noreply.github.com>
2021-06-19 03:33:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,104 @@
+function selector() {
+  if(document.getElementById("blind-snakes").checked && document.getElementById("images").checked){
+    location.href='#blind-snake-img'
+    }
+  else if(document.getElementById("blind-snakes").checked){
+    location.href='#blind-snake'
+    }
+  else if(document.getElementById("dwarf-boas").checked && document.getElementById("images").checked){
+    location.href='#dwarf-boa-img'
+    }
+  else if(document.getElementById("dwarf-boas").checked){
+    location.href='#dwarf-boa'
+    }
+  else if(document.getElementById("shield-tailed-snakes").checked && document.getElementById("images").checked){
+    location.href='#shield-tailed-snake-img'
+    }
+  else if(document.getElementById("shield-tailed-snakes").checked){
+    location.href='#shield-tailed-snake'
+    }
+  else if(document.getElementById("pythons").checked && document.getElementById("images").checked){
+    location.href='#python-img'
+    }
+  else if(document.getElementById("pythons").checked){
+    location.href='#python'
+    }
+  else if(document.getElementById("boas").checked && document.getElementById("images").checked){
+    location.href='#boa-img'
+    }
+  else if(document.getElementById("boas").checked){
+    location.href='#boa'
+    }
+  else if(document.getElementById("old-world-boas").checked && document.getElementById("images").checked){
+    location.href='#old-world-boa-img'
+    }
+  else if(document.getElementById("old-world-boas").checked){
+    location.href='#old-world-boa'
+    }
+  else if(document.getElementById("vipers").checked && document.getElementById("images").checked){
+    location.href='#viper-img'
+    }
+  else if(document.getElementById("vipers").checked){
+    location.href='#viper'
+    }
+  else if(document.getElementById("mud-snakes").checked && document.getElementById("images").checked){
+    location.href='#mud-snake-img'
+    }
+  else if(document.getElementById("mud-snakes").checked){
+    location.href='#mud-snake'
+    }
+  else if(document.getElementById("garter-snakes").checked && document.getElementById("images").checked){
+    location.href='#garter-snake-img'
+    }
+  else if(document.getElementById("garter-snakes").checked){
+    location.href='#garter-snake'
+    }
+  else if(document.getElementById("sea-snakes").checked && document.getElementById("images").checked){
+    location.href='#sea-snake-img'
+    }
+  else if(document.getElementById("sea-snakes").checked){
+    location.href='#sea-snake'
+    }
+  else if(document.getElementById("true-cobras").checked && document.getElementById("images").checked){
+    location.href='#true-cobra-img'
+    }
+  else if(document.getElementById("true-cobras").checked){
+    location.href='#true-cobra'
+    }
+  else if(document.getElementById("coral-snakes").checked && document.getElementById("images").checked){
+    location.href='#coral-snake-img'
+    }
+  else if(document.getElementById("coral-snakes").checked){
+    location.href='#coral-snake'
+    }
+  else if(document.getElementById("mole-vipers").checked && document.getElementById("images").checked){
+    location.href='#mole-viper-img'
+    }
+  else if(document.getElementById("mole-vipers").checked){
+    location.href='#mole-viper'
+    }
+  else if(document.getElementById("pit-vipers").checked && document.getElementById("images").checked){
+    location.href='#pit-viper-img'
+    }
+  else if(document.getElementById("pit-vipers").checked){
+    location.href='#pit-viper'
+    }
+  else if(document.getElementById("reed-snakes").checked && document.getElementById("images").checked) {
+    location.href='#reed-snake-img'
+    }
+  else if(document.getElementById("reed-snakes").checked){
+    location.href='#reed-snake'
+    }
+  else if(document.getElementById("flying-snakes").checked && document.getElementById("images").checked){
+    location.href='#flying-snake-img'
+    }
+  else if(document.getElementById("flying-snakes").checked){
+    location.href='#flying-snake'
+    }
+  else if(document.getElementById("other-snakes").checked && document.getElementById("images").checked){
+    location.href='#other-snake-img'
+    }
+  else if(document.getElementById("other-snakes").checked){
+    location.href='#other-snake'
+    }
+}
